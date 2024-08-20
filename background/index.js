146f8d5cc0d5b70112bcb7bb973814453290bb6a
ivan an <withdandy09@naver.com>
2024-08-20 10:15:39 +0900

    webRequest 예시 첨부

--- background/index.js
+++ background/index.js
@@ -3,3 +3,11 @@ import { someFunction } from "./module.js";
 console.log("Background script loaded");
 
 someFunction();
+
+// chrome webRequest 예시
+chrome.webRequest.onCompleted.addListener(
+  (details) => {
+    console.log('Request completed:', details.url, 'IP:', details.ip);
+  },
+  {urls: ["<all_urls>"]}
+);
